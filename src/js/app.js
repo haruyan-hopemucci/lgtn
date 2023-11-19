2ccc46f88b460f0a7a621016f785ba53f3ca3985
haruyan-hopemucci <68041476+haruyan-hopemucci@users.noreply.github.com>
2023-11-19 22:02:47 +0900

    feat: クリップボードにコピーする処理を実装

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,23 @@ $(function (){
   document.querySelector('#paste-area').addEventListener('paste', (event) => {
 
     event.preventDefault()
+
     if (!event.clipboardData
       || !event.clipboardData.types
       || (event.clipboardData.types.length != 1)
       || (event.clipboardData.types[0] != "Files")) {
+        event.target.innerHTML = 'ペーストされたデータが画像ではありません'
       return true;
     }
   
-    const imageFile = event.clipboardData.items[0].getAsFile();
+    event.target.innerHTML = 'LGTN画像を生成しています...'
+
+    generateLGTN(event.clipboardData.items[0])
+
+  })
+
+  const generateLGTN = async function(clipboardItem) {
+    const imageFile = clipboardItem.getAsFile();
     const imgEl = document.querySelector("#pasted-image")
     const fr = new FileReader();
     fr.onload = function (e) {
@@ -20,7 +29,8 @@ $(function (){
     };
     fr.readAsDataURL(imageFile);
     imgEl.addEventListener('load', drawCanvas)
-  })
+
+  }
 
   const drawCanvas = function() {
     const imgEl = document.querySelector("#pasted-image")
@@ -31,6 +41,23 @@ $(function (){
     context.clearRect(0, 0, canvas.width, canvas.height)
     context.drawImage(imgEl, 0, 0, canvas.width, canvas.height)
     context.drawImage(lgtnEl, 0, 0, canvas.width, canvas.height)
+    copyImageToClipboard(canvas)
+  }
+
+  const copyImageToClipboard = (canvas) => {
+    const pasteArea = document.querySelector('#paste-area')
+    canvas.toBlob( blob => {
+      const item = new ClipboardItem({ 'image/png': blob})
+      navigator.clipboard.write([item])
+        .then( () => {
+          pasteArea.innerHTML = 'クリップボードにLGTN画像がコピーされました！'
+        })
+        .catch( ex => {
+          console.error(ex)
+          pasteArea.innerHTML = 'クリップボードの書き込み時にエラーが発生しました'
+        })
+    });
+
   }
 })
 
